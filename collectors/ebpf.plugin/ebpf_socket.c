4aefa4557dbc2c5de69c14a739f8935477047ef0
thiagoftsm <thiagoftsm@gmail.com>
2024-01-19 00:37:18 +0000

    ebpf_functions: Use process name from kernel ring

--- collectors/ebpf.plugin/ebpf_socket.c
+++ collectors/ebpf.plugin/ebpf_socket.c
@@ -1558,6 +1558,9 @@ static void ebpf_hash_socket_accumulator(netdata_socket_t *values, int end)
 
         if (w->external_origin)
             external_origin = NETDATA_EBPF_SRC_IP_ORIGIN_EXTERNAL;
+
+        if (!values[0].name[0] && w->name[0])
+            strncpyz(values[0].name, w->name, sizeof(w->name));
     }
 
     values[0].protocol          = (!protocol)?IPPROTO_TCP:protocol;
@@ -1753,9 +1756,10 @@ static void ebpf_update_array_vectors(ebpf_module_t *em)
         }
         uint64_t prev_period = socket_ptr->data.current_timestamp;
         memcpy(&socket_ptr->data, &values[0], sizeof(netdata_socket_t));
-        if (translate)
+        if (translate) {
             ebpf_socket_translate(socket_ptr, &key);
-        else { // Check socket was updated
+            strncpy(socket_ptr->data.name, values[0].name, sizeof(values[0].name));
+        } else { // Check socket was updated
             if (prev_period) {
                 if (values[0].current_timestamp > prev_period) // Socket updated
                     socket_ptr->last_update = update_time;
@@ -2015,6 +2019,7 @@ void ebpf_socket_fill_publish_apps(uint32_t current_pid, netdata_socket_t *ns)
 
     curr->call_udp_sent = ns->udp.call_udp_sent;
     curr->call_udp_received = ns->udp.call_udp_received;
+
 }
 
 /**
