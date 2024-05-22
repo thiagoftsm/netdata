c00a774103b617ef062ec008378adcae37d9a22c
thiagoftsm <thiagoftsm@gmail.com>
2024-05-22 00:13:26 +0000

    win_interrupts: Missing headers

--- src/collectors/windows.plugin/perflib-processor.c
+++ src/collectors/windows.plugin/perflib-processor.c
@@ -3,6 +3,10 @@
 #include "windows_plugin.h"
 #include "windows-internals.h"
 
+#define _COMMON_PLUGIN_NAME "windows.plugin"
+#define _COMMON_PLUGIN_MODULE_NAME "PerflibProcessor"
+#include "../common-contexts/common-contexts.h"
+
 struct processor {
     bool collected_metadata;
 
